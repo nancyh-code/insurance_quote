555dab64986d2ad85095a669451e35f334f3587c
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-29 03:42:53 -0300

    component AppInsurance

--- src/components/AppInsurance.jsx
+++ src/components/AppInsurance.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import Form from "./Form";
 
 const AppInsurance = () => {
   return (
     <>
       <header className="my-10">
-        <h1 className="text-white text-center text-4xl font-bold">
-          App Insurance
+        <h1 className="text-amber-100 text-center font-black">
+          Car Insurance Quotes
         </h1>
       </header>
+      <main className="bg-amber-50 md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10">
+        <Form />
+      </main>
     </>
   );
 };
