77ac26d05726db6f71b9b2bf747e5c0aaebaa039
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-29 03:41:58 -0300

    style fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   content: [
-    "./components/**/*.{html,js}",
-    "./pages/**/*.{html,js}",
-    "./index.html",
+    "./src/**/*.{html,js}",
+    "./src/**/*.{html,js,jsx}",
+    "./util/**/*.{html,js}",
   ],
-  theme: {
-    colors: {
-      principal: "#B91646",
-      secondary: "#105652",
-      tertiary: "#DFD8CA",
-      white: "#FBF3E4",
-    },
-    extend: {},
-  },
+  theme: {},
+
   plugins: [],
 };
